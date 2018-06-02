9dac6e50dec40ba343a026990a7b02240253e088
syoung125 <laura1205@naver.com>
2018-06-02 20:01:53 +0900

    내꺼 레이아웃 합쳤는데 메뉴-택배, 메뉴-커뮤니티 연결은 아직안했어!

--- app/src/main/java/com/example/tarah/apartmentmanager/Notice.java
+++ app/src/main/java/com/example/tarah/apartmentmanager/Notice.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Notice extends AppCompatActivity {
      Button ntcHome;
      TextView ntcAll,ntcDong;
      ListView noticeList,noticeList2;
+     int checkNum;
      final int requestcode=51;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +30,42 @@ public class Notice extends AppCompatActivity {
         noticeList=findViewById(R.id.noticeList);
         noticeList2=findViewById(R.id.noticeList2);
         noticeList2.setVisibility(View.GONE);
+        checkNum=intent.getIntExtra("test",0);
+        if(checkNum==70){
+            noticeList2.setVisibility(View.GONE);
+            noticeList.setVisibility(View.VISIBLE);
+
+        }
+        else if(checkNum==72){
+            noticeList.setVisibility(View.GONE);
+            noticeList2.setVisibility(View.VISIBLE);
+
+        }
+        else
+            Toast.makeText(getApplicationContext(),Integer.toString(checkNum),Toast.LENGTH_SHORT).show();
     }
     public  void addListener(){
         ntcHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Notice.this,MainActivity.class);
+                Intent intent=new Intent(Notice.this,Main3Activity.class);
                 startActivityForResult(intent,requestcode);
+            }
+        });
+
+        ntcAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                noticeList2.setVisibility(View.GONE);
+                noticeList.setVisibility(View.VISIBLE);
+            }
+        });
+        ntcDong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                noticeList.setVisibility(View.GONE);
+                noticeList2.setVisibility(View.VISIBLE);
+
             }
         });
     }
